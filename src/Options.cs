6dbe5ecb48df7134019ab32d222e9faec33ff964
Jozef Izso <jozef.izso@gmail.com>
2017-06-27 23:09:58 +0200

    Sync labels from a config file to repository

---
+++ src/Options.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace Goit.GitHubLabels
+{
+    class Options
+    {
+        [Option('r', "repo", HelpText = "GitHub repository name")]
+        public string RepositoryFullName { get; set; }
+
+        [Option('t', "token", HelpText = "GitHub authorization token")]
+        public string Token { get; set; }
+
+        [Option('c', "config", HelpText = "Configuration file with labels")]
+        public string ConfigFile { get; set; }
+
+
+        [HelpVerbOption]
+        public string GetUsage(string verb)
+        {
+            return HelpText.AutoBuild(this, verb);
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using CommandLine;
+using Newtonsoft.Json.Linq;
 using Octokit;
+using Octokit.Internal;
 
 namespace Goit.GitHubLabels
 {
@@ -9,24 +15,14 @@ namespace Goit.GitHubLabels
     {
         public static int Main(string[] args)
         {
-            if (args.Length != 1)
+            var options = new Options();
+            if (!Parser.Default.ParseArguments(args, options))
             {
-                Console.WriteLine("Usage: gitlabels.exe <user>/<repo>");
-                return 1;
+                Console.WriteLine(options.GetUsage(null));
+                return Parser.DefaultExitCodeFail;
             }
 
-            var arg1 = args[0];
-            if (!arg1.Contains("/"))
-            {
-                Console.WriteLine("Usage: gitlabels.exe <user>/<repo>");
-                return 1;
-            }
-
-            var x = arg1.Split('/');
-            var username = x[0];
-            var repoName = x[1];
-
-            MainAsync(username, repoName).GetAwaiter().GetResult();
+            MainAsync(options).GetAwaiter().GetResult();
 
             if (Debugger.IsAttached)
             {
@@ -36,16 +32,79 @@ namespace Goit.GitHubLabels
             return 0;
         }
 
-        private static async Task MainAsync(string username, string repoName)
+        private static async Task MainAsync(Options options)
         {
-            var github = new GitHubClient(new ProductHeaderValue("gitlabels.exe"));
-            var labels = await github.Issue.Labels.GetAllForRepository(username, repoName);
+            var tmp = options.RepositoryFullName.Split('/');
+            var username = tmp[0];
+            var repoName = tmp[1];
 
-            Console.WriteLine($"Repository: {username}/{repoName}");
-            foreach (var label in labels)
+            var credentials = new InMemoryCredentialStore(new Credentials(options.Token));
+            var github = new GitHubClient(new ProductHeaderValue("gitlabels.exe"), credentials);
+            var repo = await github.Repository.Get(username, repoName);
+            var repoLabels = await github.Issue.Labels.GetAllForRepository(username, repoName);
+            var configLabels = LoadLabelsFromConfig(options.ConfigFile);
+
+            Console.WriteLine($"Syncing repository: {username}/{repoName}");
+
+
+            foreach (var repoLabel in repoLabels)
             {
-                Console.WriteLine($"  {label.Name}: #{label.Color}");
+                var configLabel = configLabels.FirstOrDefault(l => l.Name == repoLabel.Name);
+
+                if (configLabel == null)
+                {
+                    await github.Issue.Labels.Delete(repo.Id, repoLabel.Name);
+                    Console.WriteLine($"  - {repoLabel.Name}");
+                    continue;
+                }
+
+                if (!String.Equals(configLabel.Color, repoLabel.Color, StringComparison.OrdinalIgnoreCase))
+                {
+                    var update = new LabelUpdate(repoLabel.Name, configLabel.Color);
+                    await github.Issue.Labels.Update(repo.Id, repoLabel.Name, update);
+                    Console.WriteLine($"  * {repoLabel.Name}");
+                }
             }
+
+            foreach (var configLabel in configLabels)
+            {
+                if (!repoLabels.Any(l => String.Equals(l.Name, configLabel.Name, StringComparison.OrdinalIgnoreCase)))
+                {
+                    await github.Issue.Labels.Create(repo.Id, configLabel);
+                    Console.WriteLine($"  + {configLabel.Name}");
+                }
+            }
+
+            Console.WriteLine("Done.");
+        }
+
+        private static IList<NewLabel> LoadLabelsFromConfig(string configFile)
+        {
+            var filename = Path.GetFullPath(configFile);
+
+            if (!File.Exists(filename))
+            {
+                throw new InvalidOperationException($"Configuration file with labels was not found. Path: {filename}");
+            }
+
+            var labels = new List<NewLabel>();
+
+            var json = File.ReadAllText(filename);
+            var config = JArray.Parse(json);
+            foreach (var label in config.Values<JObject>())
+            {
+                var name = label["name"].Value<string>();
+                var color = label["color"].Value<string>();
+                if (color[0] == '#')
+                {
+                    color = color.Substring(1);
+                }
+
+                var lbl = new NewLabel(name, color);
+                labels.Add(lbl);
+
+            }
+            return labels;
         }
     }
 }
