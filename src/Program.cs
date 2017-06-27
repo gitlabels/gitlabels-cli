20751da3e586233d5688a3a04537051c92b49d05
Jozef Izso <jozef.izso@gmail.com>
2017-06-27 21:44:37 +0200

    Create project for gitlabels app

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Goit.GitHubLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("gitlabels.exe");
+        }
+    }
+}
---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("gitlabels")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("gitlabels")]
+[assembly: AssemblyCopyright("Copyright © 2017 Jozef Izso")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("e7417d92-fba3-4b4b-a64d-1ebbee030268")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
